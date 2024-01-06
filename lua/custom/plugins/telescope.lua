8665d004bbcf74f3b93d3f3c416a454b5ddb73cd
Will Shivers <tayloe93@gmail.com>
2024-01-05 22:34:14 -0600

    Update telescope layout and keymaps

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -1,7 +1,18 @@
+require('telescope').setup({
+  defaults = {
+    layout_strategy = 'vertical',
+  }
+})
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n','<leader>gf', builtin.git_files, {})
+
 return {
   'nvim-telescope/telescope.nvim', branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'BurntSushi/ripgrep'
+    'BurntSushi/ripgrep',
+    'nvim-tree/nvim-web-devicons'
   }
 }
