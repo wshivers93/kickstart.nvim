5c3519ffb994655a99008d4b56daff7883c698f1
Will Shivers <tayloe93@gmail.com>
2024-09-05 00:04:52 -0500

    Fix outdated which-key spec

--- init.lua
+++ init.lua
@@ -113,7 +113,6 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -524,22 +523,24 @@ local on_attach = function(_, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register {
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-}
--- register which-key VISUAL mode
--- required for visual <leader>hs (hunk stage) to work
-require('which-key').register({
-  ['<leader>'] = { name = 'VISUAL <leader>' },
-  ['<leader>h'] = { 'Git [H]unk' },
-}, { mode = 'v' })
+local wk = require('which-key')
+wk.add ({
+  { '<leader>c', name = '[C]ode' },
+  { '<leader>d', name = '[D]ocument' },
+  {'<leader>g', name = '[G]it' },
+  {'<leader>h', name = 'Git [H]unk' },
+  {'<leader>r', name = '[R]ename' },
+  {'<leader>s', name = '[S]earch' },
+  {'<leader>t', name = '[T]oggle' },
+  {'<leader>w', name = '[W]orkspace' },
+  {
+    -- register which-key VISUAL mode
+    -- required for visual <leader>hs (hunk stage) to work
+    mode = { "v" },
+    {'<leader>', name = 'VISUAL <leader>' },
+    {'<leader>h', name = 'Git [H]unk' },
+  }
+})
 
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
