c02576f55e647c3aff87305d0b5f5572292abde5
Will Shivers <gshivewi@amazon.com>
2025-04-18 16:25:25 +0000

    Remove default theme

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -35,7 +35,8 @@ require('lazy').setup({
 
   require 'kickstart/plugins/cmp',
 
-  require 'kickstart/plugins/tokyonight',
+  -- change made, removed default theme
+  --require 'kickstart/plugins/tokyonight',
 
   require 'kickstart/plugins/todo-comments',
 
