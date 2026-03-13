30ff14a9b4d2a28d4cfa9ea02508137af8df962c
Will Shivers <tayloe93@gmail.com>
2026-03-13 10:59:47 -0500

    Add prettier to conform plugin

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -33,6 +33,8 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
